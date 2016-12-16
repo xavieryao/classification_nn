c22922c7ed180e072c4423754f9c07232442b4ca
Xavier Yao <xavieryao@live.cn>
2016-12-16 23:09:39 +0800

    missing parenthesis

--- predict.py
+++ predict.py
@@ -11,7 +11,7 @@ class_map = ['dalitang', 'erxiaomen', 'xuetang', 'zhulou']
 def classify(filename):
     img = load_img(filename, target_size=(150, 150))
     arr = img_to_array(img)
-    x = np.zeros(1, 3, 150, 150)
+    x = np.zeros((1, 3, 150, 150))
     x[0] = arr
     r = net.predict(x, batch_size=1)
     return np.argmax(r)
@@ -22,4 +22,4 @@ if __name__ == '__main__':
     parser.add_argument('-p', "--picture")
     args = parser.parse_args()
     clz = classify(args.picture)
-    print("Belongs to {}".format(class_map[clz])
+    print("Belongs to {}".format(class_map[clz]))
